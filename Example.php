f7f151777c3d5c77e106423185769159eb5abfb2
DrRoach <ryandeas1@gmail.com>
2015-03-23 00:56:43 +0000

    Added some info and extra param

--- Example.php
+++ Example.php
@@ -5,13 +5,17 @@ This is an example of using the SwatchGenerator tool.
 <h1>Swatch Generator</h1>
 
 <?php
+$start = microtime(true);
 require_once 'Generate.php';
 new Generate([
-    'image' => 'snorkel.jpg',
+    'image' => 'rgb.png',
     'swatch' => [
         'width' => 200,
         'height' => 200
     ],
     'colour' => 'blue',
-    'file' => 'swatch'
+    'file' => 'swatch',
+    'accuracy' => 20
 ]);
+$end = microtime(true);
+echo 'Total = ' . ($end - $start);
