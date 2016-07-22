162ae73f2a89bdbb20cb3f815a5bec3ee70c3b90
DrRoach <ryandeas1@gmail.com>
2016-07-22 13:05:47 +0000

    Better comments

--- Example.php
+++ Example.php
@@ -6,6 +6,10 @@ This is an example of using the SwatchGenerator tool.
 
 <?php
 $start = microtime(true);
+
+/**
+ * Only code required to generate a swatch
+ */
 require_once 'Generate.php';
 new Generate([
     'image' => 'rgb.png',
@@ -17,6 +21,13 @@ new Generate([
     'file' => 'swatch',
     'accuracy' => 1
 ]);
+
+
+/**
+ * Benchmarking to see how image creation is performing. The dream is for a swatch to
+ * be generated in < 0.5s even when accuracy is set to `1`. At the moment, `imagecreatefrompng()`
+ * takes 0.3s to load so that would leave 0.2s for everything else to happen.
+ */
 $end = microtime(true);
 echo 'Total = '.($end - $start);
 
